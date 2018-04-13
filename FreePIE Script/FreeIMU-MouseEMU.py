939f274c12dad26bc8b417ae1be858c9f0929973
catzy007 <38147269+catzy007@users.noreply.github.com>
2018-04-13 14:42:53 +0700

    set absolute center cursor, relative center and some comments

--- FreePIE Script/FreeIMU-MouseEMU.py
+++ FreePIE Script/FreeIMU-MouseEMU.py
@@ -1,3 +1,6 @@
+import ctypes
+from ctypes import windll
+
 def update():
    global yaw
    global roll
@@ -10,9 +13,12 @@ if starting:
    yaw = 0
    roll = 0
    pitch = 0
+   rsltx = 0
+   rslty = 0
    enabled = False
    freeImu.update += update
 
+#show yaw pitch roll on freepie
 diagnostics.watch(yaw)
 diagnostics.watch(roll)
 diagnostics.watch(pitch)
@@ -20,12 +26,33 @@ diagnostics.watch(pitch)
 deltaYaw = filters.delta(yaw)
 deltaPitch = filters.delta(pitch)
 deltaRoll = filters.delta(roll)
-   
+
+#update mouse movement if enabled
 if (enabled):
-   mouse.deltaX = -deltaYaw*10
-   mouse.deltaY = -deltaPitch*10
+   rsltx += -deltaYaw*multiplier
+   rslty += -deltaPitch*multiplier
+   mouse.deltaX = -deltaYaw*multiplier
+   mouse.deltaY = -deltaPitch*multiplier
 
-toggle = keyboard.getPressed(Key.Z)
+#set key z,x,c
+toggle1 = keyboard.getPressed(Key.Z)
+toggle2 = keyboard.getPressed(Key.X)
+toggle3 = keyboard.getPressed(Key.C)
 
-if toggle:
+#enable mouse movement based on sensors key=z
+if toggle1:
+   multiplier = 10 #change value to adjust sensitivity default=10
    enabled = not enabled
+   
+#reset position to relative center key=x
+if toggle2:
+   mouse.deltaX = 0-rsltx
+   mouse.deltaY = 0-rslty
+   rsltx = 0
+   rslty = 0
+   
+#reset cursor position to absolute center of screen key=c
+if toggle3:
+   resox = windll.user32.GetSystemMetrics(0)
+   resoy = windll.user32.GetSystemMetrics(1)
+   ctypes.windll.user32.SetCursorPos(resox/2, resoy/2)
